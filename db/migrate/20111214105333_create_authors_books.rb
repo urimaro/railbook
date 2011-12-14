35b73ee994fa1afe6d7d5179e8662074fe609483
Goh Matsumoto <urimaro@gmail.com>
2011-12-14 21:03:13 +0900

    modify not to create "id" column, "create_at" column and "update_at" column

--- db/migrate/20111214105333_create_authors_books.rb
+++ db/migrate/20111214105333_create_authors_books.rb
@@ -1,10 +1,8 @@
 class CreateAuthorsBooks < ActiveRecord::Migration
   def change
-    create_table :authors_books do |t|
+    create_table :authors_books, :id => false do |t|
       t.references :author
       t.references :book
-
-      t.timestamps
     end
     add_index :authors_books, :author_id
     add_index :authors_books, :book_id
